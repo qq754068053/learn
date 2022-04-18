1144a53a845aeeac11651b50fbd01860aac74ee3
LiangGang <lianggang@LiangGangdeMacBook-Pro.local>
2022-04-18 22:17:27 +0800

    add leetcode question 386

---
+++ leetcode/386.py
@@ -0,0 +1,20 @@
+class Solution:
+    def lexicalOrder(self, n: int) -> list:
+        res = []
+
+        def dfs(x: int) -> None:
+            if x <= n:
+                res.append(x)
+                for i in range(0, 10):
+                    dfs(x * 10 + i)
+
+        for j in range(1, 10):
+            dfs(j)
+
+        return res
+
+
+if __name__ == '__main__':
+    s = Solution()
+    res = s.lexicalOrder(30)
+    print(res)
